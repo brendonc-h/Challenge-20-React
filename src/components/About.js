5e6416f8d21f77d1357fdf7bf883bf975c80a62d
Brendon Allen Curry-Hobbs <46946074+brendonc-h@users.noreply.github.com>
2022-12-04 17:54:04 -0700

    headshot commit

--- src/components/About.js
+++ src/components/About.js
@@ -27,11 +27,11 @@ export default function About() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          {/* <img
+          <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
-          /> */}
+            src="./headshot.jpeg"
+          />
         </div>
       </div>
     </section>
