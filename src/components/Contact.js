57bc053aa4d9da3947d72fb77146c3f20ec59070
Brendon Allen Curry-Hobbs <46946074+brendonc-h@users.noreply.github.com>
2022-12-04 18:06:20 -0700

    updating contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,7 +28,7 @@ export default function Contact() {
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
+          {/* <iframe
             width="100%"
             height="100%"
             title="map"
@@ -37,8 +37,9 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://github.com/brendonc-h"
-          />
+            src="
+            https://git.heroku.com/brendonsportfolio.git"
+          /> */}
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
@@ -59,7 +60,8 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">307-871-8717</p>
+              <p className="leading-relaxed"> 
+              307-871-8717 </p>
             </div>
           </div>
         </div>
